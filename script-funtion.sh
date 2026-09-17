80cb6bc33cdfa41ffca69ba17c0fad50b5b572a5
Maycon Carvalho <83347583+maycon1986@users.noreply.github.com>
2026-09-17 11:15:33 -0300

    Modify project ID and service account in script
    Updated project ID and service account placeholders in deployment script.

--- script-funtion.sh
+++ script-funtion.sh
@@ -3,8 +3,8 @@ gcloud functions deploy alertas-findings-scc \
   --gen2 \
   --runtime=python312 \
   --region=us-east4 \
-  --project=mgmt-movti-interno \
+  --project=<projeto id> \
   --trigger-topic=topic-findings-scc \
   --source=./api  \
   --entry-point=receber_mensagem \
-  --service-account=sa-apis-alertas-movti@mgmt-movti-interno.iam.gserviceaccount.com
+  --service-account=<service account>
